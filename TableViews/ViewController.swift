5c0f9353f13b11cf4bbc4f5f31ca82aefdca0cd7
Alumno <ulsa@JesusCorralMAAAAC.local>
2019-09-17 14:28:03 -0700

    Tiene errores los constrains

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    let alumnos = ["Gabriel", "Ana Lourdes", "Joel", "Trejo", "Araceli", "Karen"]
-    
+    var alumnos : [Alumno] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+
+        alumnos.append(Alumno(nombre: "AMLO", apellido: "ZOOFILICO", matricula: "ABC123"))
+        alumnos.append(Alumno(nombre: "ROSA", apellido: "MELANO", matricula: "BCD123"))
+        alumnos.append(Alumno(nombre: "CESAR", apellido: "REA", matricula: "DFG123"))
+        alumnos.append(Alumno(nombre: "BAILA", apellido: "BAILAESTACUMBIA", matricula: "XYZ123", correo: "XD@gmail.com"))
+
+
+
     }
     
     //Numero de secciones (siempre 1)
@@ -32,9 +37,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //Contenido de cada celda
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno")
+       let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno") as? CeldaAlumnoController
         
+        celda?.lblNombre.text = alumnos[indexPath.row].nombre
+        celda?.lblApellido.text = alumnos[indexPath.row].apellido
+        celda?.lblCorreo.text = alumnos[indexPath.row].correo
+        celda?.lblMatricula.text = alumnos[indexPath.row].matricula
+
+
         return celda!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if(alumnos[indexPath.row].correo != nil){
+            return 120
+        }
+        return 80
     }
     
 }
